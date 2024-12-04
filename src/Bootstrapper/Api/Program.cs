43a11aacb37a83489ee38cf093d85bbfa2ff7f08
Corinne Kessler <corinne.kessler@apostrophgroup.com>
2024-12-04 17:18:21 +0100

    Added dependency injection, shared dependencies, Aggretate Interface and domain events

--- src/Bootstrapper/Api/Program.cs
+++ src/Bootstrapper/Api/Program.cs
@@ -2,6 +2,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the dependency injection container
 
+builder.Services
+    .AddCatalogModule(builder.Configuration)
+    .AddBasketModule(builder.Configuration)
+    .AddOrderingModule(builder.Configuration);
 
 // Register custom services (later)
 
@@ -10,4 +14,11 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline
 
+// Invoke extension methods for configuring the HTTP request pipeline for each module
+app 
+    .UseCatalogModule()
+    .UseBasketModule()
+    .UseOrderingModule();
+
+            
 app.Run();
