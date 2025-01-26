8a94d2429e97b9333b6fbf3e80af4a58aa2ea053
Corinne Kessler <corinne.kessler@apostrophgroup.com>
2025-01-26 15:24:17 +0100

    Added audit fields for database seeding and updates, added interceptors for some initial domain events (product creation)

--- src/Modules/Catalog/Catalog/CatalogModule.cs
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Shared.Data.Interceptors;
+using System.ComponentModel;
 
 namespace Catalog
 {
@@ -11,15 +14,25 @@ namespace Catalog
             // Api Endpoint services
 
             // Application Use Case services
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            });
 
             // Data - Infrastructure services
             var connectionString = configuration.GetConnectionString("Database");
 
-            services.AddDbContext<CatalogDbContext>(options =>
-                options.UseNpgsql(connectionString));
+            services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+            services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+            services.AddDbContext<CatalogDbContext>((sp, options) =>
+            {
+                options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+                options.UseNpgsql(connectionString);
+            });
 
             services.AddScoped<IDataSeeder, CatalogDataSeeder>();
-            
+
             return services;
         }
 
