0d90dbf770770b864d19df50bc21216e7176c4a4
Corinne Kessler <corinne.kessler@apostrophgroup.com>
2025-02-12 17:40:49 +0100

    Implemented all Endpoints in the Catalog Module, registered ICarter Module into the DI

--- src/Bootstrapper/Api/Program.cs
+++ src/Bootstrapper/Api/Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the dependency injection container
 
+builder.Services.AddCarterWithassemblies(typeof(CatalogModule).Assembly);
+
 builder.Services
     .AddCatalogModule(builder.Configuration)
     .AddBasketModule(builder.Configuration)
@@ -13,6 +15,7 @@ builder.Services
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
+app.MapCarter();
 
 // Invoke extension methods for configuring the HTTP request pipeline for each module
 app 
